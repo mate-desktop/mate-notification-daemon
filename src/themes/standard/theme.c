3adfad8097615f179f7c96f27ccd8ed7f9ba5a15
Victor Kareh <vkareh@redhat.com>
2025-08-20 21:54:22 -0400

    themes: Display countdown on non-persistent notification messages
    Countdown was only visible for notifications that had action buttons.
    This change adds a 'show-countdown' gsetting to display it instead on
    all non-persisntent notifications.

--- src/themes/standard/theme.c
+++ src/themes/standard/theme.c
@@ -100,6 +100,8 @@ void move_notification(GtkWidget *nw, int x, int y);
 void set_notification_timeout(GtkWindow *nw, glong timeout);
 void set_notification_hints(GtkWindow *nw, GVariant *hints);
 void notification_tick(GtkWindow *nw, glong remaining);
+static void create_pie_countdown(WindowData* windata);
+static gboolean on_countdown_draw (GtkWidget *widget, cairo_t *cr, WindowData *windata);
 
 //#define ENABLE_GRADIENT_LOOK
 
@@ -835,6 +837,27 @@ void set_notification_hints(GtkWindow *nw, GVariant *hints)
 	}
 }
 
+static void create_pie_countdown(WindowData* windata)
+{
+	if (windata->pie_countdown != NULL)
+		return; /* Already created */
+
+	windata->pie_countdown = gtk_drawing_area_new();
+	gtk_widget_set_halign (windata->pie_countdown, GTK_ALIGN_END);
+	gtk_widget_set_valign (windata->pie_countdown, GTK_ALIGN_CENTER);
+	gtk_widget_show(windata->pie_countdown);
+
+	#if GTK_CHECK_VERSION (4,0,0)
+		gtk_widget_add_css_class (windata->pie_countdown, "countdown");
+	#else
+		gtk_style_context_add_class (gtk_widget_get_style_context (windata->pie_countdown), "countdown");
+	#endif
+
+	gtk_box_pack_end (GTK_BOX (windata->actions_box), windata->pie_countdown, FALSE, TRUE, 0);
+	gtk_widget_set_size_request(windata->pie_countdown, PIE_WIDTH, PIE_HEIGHT);
+	g_signal_connect(G_OBJECT(windata->pie_countdown), "draw", G_CALLBACK(on_countdown_draw), windata);
+}
+
 void set_notification_timeout(GtkWindow* nw, glong timeout)
 {
 	WindowData* windata = g_object_get_data(G_OBJECT(nw), "windata");
@@ -842,6 +865,22 @@ void set_notification_timeout(GtkWindow* nw, glong timeout)
 	g_assert(windata != NULL);
 
 	windata->timeout = timeout;
+
+	/* Check if we should show countdown for all timed notifications */
+	if (timeout > 0)
+	{
+		GSettings *gsettings = g_settings_new ("org.mate.NotificationDaemon");
+		gboolean show_countdown = g_settings_get_boolean (gsettings, "show-countdown");
+		g_object_unref (gsettings);
+
+		if (show_countdown)
+		{
+			/* Ensure actions_box is visible for countdown */
+			gtk_widget_show(windata->actions_box);
+			update_content_hbox_visibility(windata);
+			create_pie_countdown(windata);
+		}
+	}
 }
 
 void notification_tick(GtkWindow* nw, glong remaining)
@@ -1066,24 +1105,7 @@ void add_notification_action(GtkWindow* nw, const char* text, const char* key, A
 	{
 		gtk_widget_show(windata->actions_box);
 		update_content_hbox_visibility(windata);
-
-		/* Don't try to re-add a pie_countdown */
-		if (!windata->pie_countdown) {
-			windata->pie_countdown = gtk_drawing_area_new();
-			gtk_widget_set_halign (windata->pie_countdown, GTK_ALIGN_END);
-			gtk_widget_set_valign (windata->pie_countdown, GTK_ALIGN_CENTER);
-			gtk_widget_show(windata->pie_countdown);
-
-			#if GTK_CHECK_VERSION (4,0,0)
-				gtk_widget_add_css_class (windata->pie_countdown, "countdown");
-			#else
-				gtk_style_context_add_class (gtk_widget_get_style_context (windata->pie_countdown), "countdown");
-			#endif
-
-			gtk_box_pack_end (GTK_BOX (windata->actions_box), windata->pie_countdown, FALSE, TRUE, 0);
-			gtk_widget_set_size_request(windata->pie_countdown, PIE_WIDTH, PIE_HEIGHT);
-			g_signal_connect(G_OBJECT(windata->pie_countdown), "draw", G_CALLBACK(on_countdown_draw), windata);
-		}
+		create_pie_countdown(windata);
 	}
 
 	if (windata->action_icons) {
