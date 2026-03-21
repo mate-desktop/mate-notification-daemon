a7ba9a1701eddd9c9e55a4a88b0ea136440cc539
Victor Kareh <vkareh@redhat.com>
2026-03-21 12:06:06 -0400

    daemon: Fix NotificationClosed signal reason for close button
    When clicking the close button on a notification, the D-Bus signal
    incorrectly sent reason=1 (expired) instead of reason=2 (dismissed).
    
    Fixed by setting a flag on the notification window in the close button
    callback before destroying it, and checking this flag in the callback to
    send the correct close reason.
    
    Fixes #129

--- src/themes/standard/theme.c
+++ src/themes/standard/theme.c
@@ -642,6 +642,13 @@ static gboolean activate_link(GtkLabel* label, const char* url, WindowData* wind
 	return TRUE;
 }
 
+static void
+close_button_clicked_cb(GtkButton* button, GtkWidget* win)
+{
+	g_object_set_data(G_OBJECT(win), "_user_closed", GINT_TO_POINTER(1));
+	gtk_widget_destroy(win);
+}
+
 GtkWindow* create_notification(UrlClickedCb url_clicked)
 {
 	GtkWidget* spacer;
@@ -754,7 +761,7 @@ GtkWindow* create_notification(UrlClickedCb url_clicked)
 	gtk_button_set_relief(GTK_BUTTON(close_button), GTK_RELIEF_NONE);
 	gtk_container_set_border_width(GTK_CONTAINER(close_button), 0);
 	//gtk_widget_set_size_request(close_button, 20, 20);
-	g_signal_connect_swapped(G_OBJECT(close_button), "clicked", G_CALLBACK(gtk_widget_destroy), win);
+	g_signal_connect(G_OBJECT(close_button), "clicked", G_CALLBACK(close_button_clicked_cb), win);
 
 	atkobj = gtk_widget_get_accessible(close_button);
 	atk_action_set_description(ATK_ACTION(atkobj), 0,
